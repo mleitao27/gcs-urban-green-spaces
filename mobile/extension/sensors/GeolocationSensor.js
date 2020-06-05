e3ed9a568268c5405df622d5f3e14c1ea9a86c4c
Miguel Leitão <mleitao27@gmail.com>
2020-06-05 01:09:51 +0100

    Geolocation fix.

--- mobile/extension/sensors/GeolocationSensor.js
+++ mobile/extension/sensors/GeolocationSensor.js
@@ -17,38 +17,44 @@ const GeolocationSensor = props => {
 
     // Get geolocation when component is used
     useEffect(() => {
-        (async () => {
-            // Only gets location once
-            if (location === null)
-                // Get location
-                await getLocationAsync();
-            // Only sends answer if there is data to send
-            if (geocode !== null && location !== null)
-                // Send location data(geocode + lat + long) to form component
-                props.onChange(props.pageIndex, props.index, {sensor: 'geolocation', data: { ...geocode[0], ...location }});
-        })();
+        let cancel = false;
+
+        const getLocationAsync = async () => {
+            // Gets permissions
+            let { status } = await Permissions.askAsync(Permissions.LOCATION);
+            if (status !== 'granted') {
+                setErrorMessage('Permission to access location was denied');
+            }
+    
+            // Gets coordinates
+            let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest });
+            const { latitude, longitude } = location.coords;
+            await getGeocodeAsync({ latitude, longitude });
+            if (cancel === false) setLocation({ latitude, longitude });
+        };
+    
+        // Get geocode
+        const getGeocodeAsync = async (location) => {
+            let geocode = await Location.reverseGeocodeAsync(location);
+            if (cancel === false) setGeocode(geocode);
+        };
+
+        // Only gets location once
+        if (location === null && geocode === null)
+            // Get location
+            getLocationAsync();
+
+        // Only sends answer if there is data to send
+        if (geocode !== null && location !== null)
+            // Send location data(geocode + lat + long) to form component
+            props.onChange(props.pageIndex, props.index, {sensor: 'geolocation', data: { ...geocode[0], ...location }});
+        
+            return () => {
+            cancel = true;
+        };
     }, [location, geocode]);
 
     // Get geolocation
-    getLocationAsync = async () => {
-        // Gets permissions
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-            setErrorMessage('Permission to access location was denied');
-        }
-
-        // Gets coordinates
-        let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest });
-        const { latitude, longitude } = location.coords;
-        await getGeocodeAsync({ latitude, longitude });
-        if (location === null) setLocation({ latitude, longitude });
-    };
-
-    // Get geocode
-    getGeocodeAsync = async (location) => {
-        let geocode = await Location.reverseGeocodeAsync(location);
-        if (geocode === null) setGeocode(geocode);
-    };
 
     // Render dummy view
     return (
