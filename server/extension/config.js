9b1b36754779e59e585e82382bdeb7da7a89ab42
Miguel Leitão <mleitao27@gmail.com>
2020-06-20 11:45:44 +0100

    Some vars to config.

--- server/extension/config.js
+++ server/extension/config.js
@@ -6,9 +6,17 @@ const admin = [
     }
 ];
 
-// Set cache timeout
+// Set cache timeout in seconds
 const userTimeout = 3600;
+
+// Interval to consider that user has answered recently in seconds
+const recentAnswer = 3600;
+
+// Offset to include an UGS in the user proximity list in meters
+const inUgsOffset = 1000;
 
 // Exports
 exports.admin = admin;
 exports.userTimeout = userTimeout;
+exports.recentAnswer = recentAnswer;
+exports.inUgsOffset = inUgsOffset;
