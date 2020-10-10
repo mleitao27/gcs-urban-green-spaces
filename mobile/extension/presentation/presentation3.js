90dc9e81906c151196e293a09b197ab757090e4a
mleitao27 <mleitao27@gmail.com>
2020-10-10 02:50:07 +0100

    Finishing tutorial v1.

--- mobile/extension/presentation/presentation3.js
+++ mobile/extension/presentation/presentation3.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, Image, ScrollView } from 'react-native';
 
 // Window width and height used for styling purposes
 const windowWidth = Dimensions.get('window').width;
@@ -7,20 +7,61 @@ const windowHeight = Dimensions.get('window').height;
 
 const presentation3 = props => {
     return (
-        <View style={styles.container}>
-            <Text style={{...styles.text, ...styles.title}}>
-                Survey
-            </Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={{...styles.text, ...styles.title}}>
+                    Survey
+                </Text>
+                <Text style={styles.text}>
+                    The survey screen is the most relevant screen in the app.
+                    Because it is here where you, the user, will be able to shared with the researchers' team your opinion about the UGS.
+                    The answers given in these surveys are the reason for the existence of this app, so it is asked of you to answer as honestly and thoughtfully as possible.
+                    This screen is divided into 2 sub screens.
+                    The form screen can be accessed by pressing the button in the section 1.
+                </Text>
+                <Image
+                    style={styles.image1}
+                    source={require('../assets/survey1.png')}
+                />
+                <Text style={styles.text}>
+                    The form screen consists of a map where the user can see its geographical position as well as the UGS marked in the map with a green circumference as shown in the section 2.
+                    In the screen there will also appear different questions in the section 3 to which you must answer in order to complete the survey.
+                    There is also a button that allows to change the map's type (section 4).
+                </Text>
+                <Image
+                    style={styles.image2}
+                    source={require('../assets/survey2.png')}
+                />
+                <Text style={styles.text}>
+                    By clicking in the button in section 5 you will be redirected to the mapping sub screen.
+                    This screen is also dedicated to the collection of data about UGS.
+                    But this time the user is asked to mark relevant places inside each UGS.
+                    These markers can either be associated with the user's feelings, the weather or the UGS elements.
+                </Text>
+                <Image
+                    style={styles.image3}
+                    source={require('../assets/survey3.png')}
+                />
+                <Text style={styles.text}>
+                    The screen contains a map similar to the one presented before but where will be presented the marked places like in section 6.
+                    Below (section 7) are the buttons that must be pressed in order to mark a specific place.
+                </Text>
+                <Image
+                    style={styles.image4}
+                    source={require('../assets/survey4.png')}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
         padding: windowWidth * 0.05,
+    },  
+    content: {
+        paddingBottom:windowWidth * 0.1
     },
     text: {
         textAlign:'center',
@@ -30,6 +71,22 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: windowHeight*0.05
+    },
+    image1: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*0.26,
+    },
+    image2: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*1.79,
+    },
+    image3: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*0.26,
+    },
+    image4: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*2.09,
     }
 });
 
--- mobile/extension/presentation/presentation4.js
+++ mobile/extension/presentation/presentation4.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, ScrollView, Image } from 'react-native';
 
 // Window width and height used for styling purposes
 const windowWidth = Dimensions.get('window').width;
@@ -7,20 +7,71 @@ const windowHeight = Dimensions.get('window').height;
 
 const presentation3 = props => {
     return (
-        <View style={styles.container}>
-            <Text style={{...styles.text, ...styles.title}}>
-                Results
-            </Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={{...styles.text, ...styles.title}}>
+                    Results
+                </Text>
+                <Text style={styles.text}>
+                    The results screen is where the user can find both the given answers and the placed markers.
+                    It is structured similarly to the survey screen.
+                    By pressing the button in the section 1 the user is taken to the answers list.
+                </Text>
+                <Image
+                    style={styles.image1}
+                    source={require('../assets/results1.png')}
+                />
+                <Text style={styles.text}>
+                    Here (section 2) is presented a list of clickable cards containing some information about the different answer given so far.
+                </Text>
+                <Image
+                    style={styles.image2}
+                    source={require('../assets/results2.png')}
+                />
+                <Text style={styles.text}>
+                    After clicking in any of the available cards it is displayed some more detailed information about that card's respective answer as it is shown in the section 3.
+                    The button in the section 4 will allow you to go back to the answers list.
+                </Text>
+                <Image
+                    style={styles.image3}
+                    source={require('../assets/results3.png')}
+                />
+                <Text style={styles.text}>
+                    The button in the section 5 redirects to a list of UGS.
+                    This list contains all the UGS where the user placed any marker.
+                </Text>
+                <Image
+                    style={styles.image4}
+                    source={require('../assets/results4.png')}
+                />
+                <Text style={styles.text}>
+                    Each UGS will be represented by a clickable card as before and demonstrated in the section 6.
+                </Text>
+                <Image
+                    style={styles.image5}
+                    source={require('../assets/results5.png')}
+                />
+                <Text style={styles.text}>
+                    Clicking in one of the mentioned UGS cards will lead to a screen containing a list of all the markers placed in that UGS.
+                    In this screen it is also possible to see some details about each individual marker.
+                    Equivalently to the answers detail screen here there is also a back button to return to the UGS list.
+                </Text>
+                <Image
+                    style={styles.image6}
+                    source={require('../assets/results6.png')}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
         padding: windowWidth * 0.05,
+    },  
+    content: {
+        paddingBottom:windowWidth * 0.1
     },
     text: {
         textAlign:'center',
@@ -30,7 +81,31 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: windowHeight*0.05
-    }
+    },
+    image1: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*0.27,
+    },
+    image2: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*1.81,
+    },
+    image3: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*2.05,
+    },
+    image4: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*0.26,
+    },
+    image5: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*0.67,
+    },
+    image6: {
+        width: windowWidth*0.8,
+        height: windowWidth*0.8*2.09,
+    },
 });
 
 export default presentation3;
