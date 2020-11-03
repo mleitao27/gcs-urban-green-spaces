fd4a9640c83547cd16a5acbb90b14b0c77adacd1
mleitao27 <mleitao27@gmail.com>
2020-11-03 16:43:59 +0000

    Removing difffeedback prints.

--- server/extension/feedback/diffFeedback1.js
+++ server/extension/feedback/diffFeedback1.js
@@ -1,5 +1,4 @@
 const diffFeedback1 = () => {
-    console.log('diffFeedback1');
 };
 
 exports.diffFeedback1 = diffFeedback1;
--- server/extension/feedback/diffFeedback3.js
+++ server/extension/feedback/diffFeedback3.js
@@ -1,5 +1,4 @@
 const diffFeedback3 = () => {
-    console.log('diffFeedback3');
 };
 
 exports.diffFeedback3 = diffFeedback3;
