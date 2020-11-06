055c2a3f8a0b699ec76ff9bc8df5de804859dfe8
mleitao27 <mleitao27@gmail.com>
2020-11-06 23:37:32 +0000

    Usability form.

--- mobile/extension/presentation/presentation1.js
+++ mobile/extension/presentation/presentation1.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, ScrollView, Linking } from 'react-native';
 
 import dictionary from '../dictionaryExtension.json';
+
+import CustomButton from '../../components/CustomButton';
 
 // Window width and height used for styling purposes
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const presentation1 = props => {
+
+    const openURL = (url) => {
+        Linking.openURL(url);
+    };
+
     return (
         <ScrollView style={styles.container}>
             <View style={styles.content}>
@@ -17,15 +24,22 @@ const presentation1 = props => {
                 <Text style={styles.text}>
                 {dictionary[props.language].PRESENTATION.PAGE[1].TEXT[1]}
                 </Text>
+                <View style={{width: '100%', alignItems: 'center', marginTop: windowHeight*0.03}}>
+                    <CustomButton
+                        title={dictionary[props.language].USABILITY_FORM}
+                        onPress={openURL.bind(this, 'https://forms.gle/rsAb5EpBmygTf2TD8')}
+                        backgroundColor={'white'}
+                        shadow={true}
+                        textColor={'black'}
+                        height={windowHeight * 0.06}
+                        width={windowWidth * 0.7}
+                        bold={true}
+                    />
+                </View>
             </View>
         </ScrollView>
     );
 };
-
-/*<Text style={{color: 'blue'}}
-      onPress={() => Linking.openURL('http://google.com')}>
-  Google
-</Text>*/
 
 const styles = StyleSheet.create({
     container: {
